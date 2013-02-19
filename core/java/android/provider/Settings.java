0454dce745f596ec592bc9c5b5ae382e0f7d019a
Lars Greiss <kufikugel@googlemail.com>
2013-02-19 01:17:53 +0100

    Custom carrier text (1/2) thanks a bunch @rukin5197
    this was based on by AOKP's use of the code
    enter nothing in the text box to revert back to default.
    
    change to original commit:
    - better integration into our wifi label feature in SystemUI
    
    Change-Id: Ib24e476d4b83abd4f5b127ed1e4addfad517462d
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2864,7 +2864,14 @@ public final class Settings {
          */
         public static final String STATUS_BAR_IME_SWITCHER = "status_bar_ime_switcher";
 
-         /**
+        /**
+         * Holds the text for the Carrier label. An empty string
+         * will bring the default text back.
+         * @hide
+         */
+        public static final String CUSTOM_CARRIER_LABEL = "custom_carrier_label";
+
+        /**
          * Expanded desktop on/off state
          * @hide
          */
